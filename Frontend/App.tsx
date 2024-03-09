52c081ce26a2c3edf32879756ba0977db22c4005
nadia3190 <nadia.tlb.90@gmail.com>
2024-03-09 11:18:14 +0100

    first commit

---
+++ Frontend/App.tsx
@@ -0,0 +1,38 @@
+
+
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenue sur mon application mobile</Text>
+      
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
